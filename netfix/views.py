ff3dba1faaa1b64abc1cc739bd90af213c2664ec
dliolios <liolios.dimitris@gmail.com>
2025-02-13 23:02:10 +0200

    starting services

--- netfix/views.py
+++ netfix/views.py
@@ -7,12 +7,12 @@ from services.models import Service
 def home(request):
     return render(request, "users/home.html", {"user": request.user})
 
-def customer_profile(request):
-    # need edit
-    user = User.objects.get(username=name)
-    services = Service.objects.filter(company=Company.objects.get(user=user)).order_by("-date")
 
-    return render(request, "users/profile.html", {"user": user, "services": services})
+def customer_profile(request, name):
+
+    user = User.objects.get(username=name)
+
+    return render(request, "users/profile.html", {"user": user, "requested_services": requested_services})
 
 
 def company_profile(request, name):
