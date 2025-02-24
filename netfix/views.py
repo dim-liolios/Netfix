cdd5edd942d3c5f8c8e4b97fa75247bc4d6bf508
dliolios <liolios.dimitris@gmail.com>
2025-02-25 00:23:37 +0200

    proly FINISHED / fixed age for customers

--- netfix/views.py
+++ netfix/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import date
 
 from users.models import User, Company, Customer
 from services.models import Service, RequestService
@@ -8,14 +9,19 @@ def home(request):
     return render(request, "users/home.html", {"user": request.user})
 
 
+def calculate_age(birth_date):
+    today = date.today()
+    return today.year - birth_date.year
+
+
 def customer_profile(request, name):
     user = User.objects.get(username=name)
-
     customer = Customer.objects.get(user=user)
 
+    user_age = calculate_age(customer.date_of_birth)
     requested_services = RequestService.objects.filter(customer=customer).order_by("-requested_date")
 
-    return render(request, "users/profile.html", {"user": user, "requested_services": requested_services})
+    return render(request, "users/profile.html", {"user": user, "requested_services": requested_services, "user_age": user_age})
 
 
 def company_profile(request, name):
